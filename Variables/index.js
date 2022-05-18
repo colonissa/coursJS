ca8d934fd36ef87a950b1c2b9bcf79a1e320ba7f
colonissa <issacolon2@gmail.com>
2022-05-18 18:00:09 +0100

    variable en javascript

---
+++ Variables/index.js
@@ -0,0 +1,30 @@
+console.log("bonjour le monde");
+// variable
+var nom;
+var prenom = "colon";
+prenom = "ISSA";
+var nombre = 5;
+var vrai = true;
+console.log(prenom);
+console.log(nom);
+console.log(nom);
+console.log(vrai);
+const TVA = 19.5;
+console.log(TVA);
+var chiffre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+console.log(chiffre);
+var lettre = ["a", "b", "c", "d"];
+console.log(lettre);
+var bouleen = ["true", "false", "true", "false", "true", "false"];
+console.log(bouleen);
+var melange = ["true", "a", 1, "false"];
+console.log(melange);
+var obj = {
+  nom: "soulouck",
+  taille: 1.96,
+  getNom: function () {
+    console.log(this.nom);
+  },
+};
+console.log(obj);
+console.log(obj.taille);
