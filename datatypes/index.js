048b5c9758f9ce2174fb75471a31751016e55089
colonissa <issacolon2@gmail.com>
2022-05-19 18:03:48 +0100

    datatype en javascript

---
+++ datatypes/index.js
@@ -0,0 +1,15 @@
+console.log("bonjour datatypes");
+var a;
+console.log(typeof a);
+var b = 2;
+console.log(typeof b);
+var c = "beone";
+console.log(typeof c);
+var d = true;
+console.log(typeof d);
+var obj = {};
+console.log(typeof obj);
+var maFonction = function () {};
+console.log(typeof maFonction);
+var r = [];
+console.log(typeof r);
